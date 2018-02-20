734da57989a88ac6dbba3e291af25c3eefc1b74d
Bhavik Sheth <bhaviksheth20@gmail.com>
2018-02-20 18:46:49 +0000

    change location of tests and test recursively in src folder

--- src/demo.spec.js
+++ src/demo.spec.js
@@ -4,7 +4,7 @@ import { shallow, configure } from "enzyme";
 
 import Adapter from "enzyme-adapter-react-16";
 
-import App from "../src/components/app";
+import App from "./components/app";
 
 configure({ adapter: new Adapter() });
 
