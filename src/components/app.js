cf6ca992839fe282e967f28ef1ed0322002d4d57
Bhavik Sheth <bhaviksheth20@gmail.com>
2018-03-11 16:45:07 +0000

    implement seed, start, and play methods

--- src/components/app.js
+++ src/components/app.js
@@ -23,6 +23,56 @@ class App extends Component {
     });
   }
 
+  seed = () => {
+    let boardCopy = arrayClone(this.state.board);
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
+        if (Math.floor(Math.random() * 4) === 0) {
+          boardCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      board: boardCopy,
+    });
+  }
+
+  start = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  play = () => {
+    const { board } = this.state;
+    const boardCopy = arrayClone(this.state.board);
+
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
+        let count = 0;
+        if (i > 0) if (board[i - 1][j]) count += 1;
+        if (i > 0 && j > 0) if (board[i - 1][j - 1]) count += 1;
+        if (i > 0 && j < this.cols - 1) if (board[i - 1][j + 1]) count += 1;
+        if (j < this.cols - 1) if (board[i][j + 1]) count += 1;
+        if (j > 0) if (board[i][j - 1]) count += 1;
+        if (i < this.rows - 1) if (board[i + 1][j]) count += 1;
+        if (i < this.rows - 1 && j > 0) if (board[i + 1][j - 1]) count += 1;
+        if (i < this.rows - 1 && this.cols - 1) if (board[i + 1][j + 1]) count += 1;
+        if (board[i][j] && (count < 2 || count > 3)) boardCopy[i][j] = false;
+        if (!board[i][j] && count === 3) boardCopy[i][j] = true;
+      }
+    }
+
+    this.setState({
+      board: boardCopy,
+      generations: this.state.generations + 1,
+    });
+  }
+
+  componentDidMount() {
+    this.seed();
+    this.start();
+  }
+
   render() {
     return (
       <div className="app">
